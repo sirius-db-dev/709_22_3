483e8b26c3c167870064ccffc6a65d7f9bc73b4d
Maksim <74344870+Rubsun@users.noreply.github.com>
2024-03-19 09:49:06 +0300

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -5,16 +5,20 @@
 
 create extension if not exists "uuid-ossp";
 
-create table if not exists fest
+
+
+drop table fest_to_person cascade;
+
+create table fest
 (
     id         uuid primary key default uuid_generate_v4(),
     title text,
-    date date,
+    date_of_fest date,
     place text
 );
 
 
-create  table if not exists person
+create  table person
 (
     id         uuid primary key default uuid_generate_v4(),
     first_name text,
@@ -22,20 +26,26 @@ create  table if not exists person
     birthdate date
 );
 
-create table if not exists fest_to_person
+create table fest_to_person
 (
     person_id uuid references person,
-    fest_id uuid references fest
+    fest_id uuid references fest,
+    primary key (person_id, fest_id)
 );
 
 
-insert into fest(title, date, place) values
+insert into fest(title, date_of_fest, place) values
 ('VK fest', '2023-02-23', 'Sirius'),
-('ВФМ', '2024-03-01', 'Sirius');
+('ВФМ', '2024-03-01', 'Sirius'),
+('Games of the future', '2024-01-11', 'Kazan');
 
 insert into person (first_name, last_name, birthdate) values
 ('Maksim', 'Nudga', '2006-02-04'),
-('Sergo', 'Telegin', '2006-10-9');
+('Sergo', 'Telegin', '2006-10-9'),
+('Bob', 'Bober', '2000-10-10'),
+('Darina', 'Nudga', '2006-02-04'),
+('Oleg', 'Olegovich', '1989-02-05'),
+('Victor','Kus', '1982-10-01');
 
 
 insert into fest_to_person (person_id, fest_id)
@@ -43,15 +53,20 @@ values
     ((select id from person where first_name = 'Maksim'),
      (select id from fest where title = 'VK fest')),
     ((select id from person where first_name = 'Sergo'),
-     (select id from fest where title = 'ВФМ'));
+     (select id from fest where title = 'ВФМ')),
+    ((select id from person where first_name = 'Darina'),
+     (select id from fest where title = 'VK fest')),
+ 	((select id from person where first_name = 'Oleg'),
+ 	 (select id from fest where title ='ВФМ'));
 
+ 	
 select
     person.id,
     person.first_name,
     person.last_name,
     person.birthdate,
     coalesce(json_agg(json_build_object(
-                      'id', fest.id, 'title', fest.title, 'date', fest.date))
+                      'id', fest.id, 'title', fest.title, 'date', fest.date_of_fest))
                         filter ( where fest.id is not null ), '[]') as fests
 from person
 left join fest_to_person ftp on person.id = ftp.person_id
@@ -62,7 +77,7 @@ group by person.id;
 select
     fest.id,
     fest.title,
-    fest.date,
+    fest.date_of_fest,
     fest.place,
     coalesce(json_agg(json_build_object(
                       'id', person.id, 'name', person.first_name, 'last_name', person.last_name))
